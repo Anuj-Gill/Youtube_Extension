479aa306dabc01d3dc36a648f1a032f05502cce9
Anuj Gill <anujgill212@gmail.com>
2023-12-07 23:09:05 +0530

    Sync storage feature implemented

--- popup.js
+++ popup.js
@@ -1,69 +1,72 @@
 // Wait for the DOM to be fully loaded
 document.addEventListener('DOMContentLoaded', function () {
 
-  // Check if the current tab is on a YouTube video page
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    const currentTab = tabs[0];
-    console.log(currentTab);
-
-    // Check if the URL is a YouTube video page using the isYouTubeVideoPage 
-    if (isYouTubeVideoPage(currentTab.url)) {
-      console.log("Entered youtube page");
-      // If on a YouTube video page, proceed with the logic
-      // - Attach event listener to handle form submission
-      document.getElementById('markItForm').addEventListener('submit', function (event) {
-        console.log("Form submitted");
-        event.preventDefault(); // Prevent the default form submission behavior
-        const noteInput = document.querySelector('#noteInput').value;
-        console.log(noteInput);
-        // Send a message to content script to request the timestamp
-        // chrome.runtime.sendMessage({ action: 'requestTimestamp', noteInput: noteInput });
-        chrome.tabs.sendMessage(currentTab.id, { action: 'requestTimestamp', noteInput: noteInput });
-        console.log("Msg sent to contentScript!");
-
       // - Implement the function to display existing timestamps
-      setInterval(() => {
+      function displayTimestamps() {
         chrome.runtime.sendMessage({ action: 'getStoredTimestamp' }, function (response) {
-          console.log("Msg received from background: ", response.storedTimestamp);
+          console.log("Msg received from background: ");
           const timestampsList = document.querySelector('.timeStampsList');
           timestampsList.innerHTML = '';
-        
-          if (response.action === 'updateTimestamp') {
-            for (let i = 0; i <= response.storedTimestamp.length; i++) {
-              const ts = response.storedTimestamp[i];
-        
+
+          if (response && response.action === 'updateTimestamp') {
+            const storedTimestamp = response.storedTimestamp;
+
+            for (let i = 0; i < storedTimestamp.length; i++) {
+              const ts = storedTimestamp[i];
+
               // Create elements
               const indexElement = document.createElement('p');
               indexElement.textContent = i + 1; // Use i + 1 to avoid displaying 1 for all entries
               indexElement.classList.add('index');
-        
+
               const timestampElement = document.createElement('p');
-              timestampElement.textContent = ts["timestamp"];
+              timestampElement.textContent = ts.timestamp;
               timestampElement.classList.add('timeStamp');
-        
+
               const noteElement = document.createElement('p');
-              noteElement.textContent = ts["noteInput"];
+              noteElement.textContent = ts.noteInput;
               noteElement.classList.add('noteAdded');
-        
+
               // Create container for each timestamp
               const timestampContainer = document.createElement('div');
               timestampContainer.appendChild(indexElement);
               timestampContainer.appendChild(timestampElement);
               timestampContainer.appendChild(noteElement);
-        
+
               // Append container to timestamps list
               timestampsList.appendChild(timestampContainer);
             }
-          } 
-          else{
-            timestampsList.innerHTML = "Your timestamps are loading"
+          } else {
+            console.log('Invalid response or no timestamps stored yet:', response);
+            timestampsList.innerHTML = 'Invalid response or no timestamps stored yet';
           }
         });
-        
-      }, 200);
+      }
       
-    });
-  }
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        const currentTab = tabs[0];
+        console.log(currentTab);
+    
+        // Check if the URL is a YouTube video page using the isYouTubeVideoPage 
+        if (isYouTubeVideoPage(currentTab.url)) {
+          console.log("Entered youtube page");
+          // If on a YouTube video page, proceed with the logic
+          // - Attach event listener to handle form submission
+          document.getElementById('markItForm').addEventListener('submit', function (event) {
+            console.log("Form submitted");
+            event.preventDefault(); // Prevent the default form submission behavior
+            const noteInput = document.querySelector('#noteInput').value;
+            console.log(noteInput);
+            // Send a message to content script to request the timestamp
+            // chrome.runtime.sendMessage({ action: 'requestTimestamp', noteInput: noteInput });
+            chrome.tabs.sendMessage(currentTab.id, { action: 'requestTimestamp', noteInput: noteInput });
+            console.log("Msg sent to contentScript!");
+
+            displayTimestamps();
+          });
+          displayTimestamps();
+
+        }
      else {
       // If not on a YouTube video page, display a message or take appropriate action
       const heading = document.querySelector('h1');
@@ -82,5 +85,4 @@ document.addEventListener('DOMContentLoaded', function () {
   function isYouTubeVideoPage(url) {
     return url.includes('youtube.com') && url.includes('/watch');
   }
-
 });
--- service_worker.js
+++ service_worker.js
@@ -1,21 +1,40 @@
 console.log("hello there, service_worker here");
-let storedTimestamp = [];
+
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   if (message.action === 'storeTimestamp') {
     console.log('Received message in background script:',sender);
     console.log(message.timestamp);
+    console.log(message.noteInput);
     // Store the timestamp received from the content script
-    storedTimestamp.push({ timestamp: message.timestamp, noteInput: message.noteInput });
-    console.log(storedTimestamp);
-  } else if (message.action === 'getStoredTimestamp') {
-    // Respond to the popup's request with the stored timestamp
-    if (storedTimestamp.length != 0) {
-      console.log("Sending correct thing");
-      sendResponse({ action: 'updateTimestamp', storedTimestamp});
-    } else {
-      console.log("sending undefined");
-      sendResponse({ action: 'Have some patience', storedTimestamp});
-    }
+    chrome.storage.sync.set({ timestamp: message.timestamp, noteInput: message.noteInput }).then(() => {
+      if (chrome.runtime.lastError) {
+        console.error("Error saving timestamps:", chrome.runtime.lastError.message);
+      } else {
+        console.log("Timestamps saved to storage.");
+      }
+    });
+  } 
+  else if (message.action === 'getStoredTimestamp') {
+    return true;
   }
+});
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.action === 'getStoredTimestamp') {
+    chrome.storage.sync.get(["timestamp", "noteInput"], function (result) {
+      console.log("Values retrieved from the storage");
+      console.log(result);
+      console.log("Timestamp value is " + result.timestamp);
+      console.log("NoteInput value is " + result.noteInput);
+      if (result.timestamp && result.noteInput) {
+        console.log("Correct data sent!!");
+        sendResponse({ action: 'updateTimestamp', storedTimestamp: [result] });
+      } else {
+        console.log('Nothing sent!');
+        sendResponse({ action: 'No timestamps stored yet' });
+      }
+    });
+    // Do not call sendResponse here; it should be called inside the chrome.storage.local.get callback
+  }
+  return true; // Indicates that the sendResponse callback will be called asynchronously
 });
